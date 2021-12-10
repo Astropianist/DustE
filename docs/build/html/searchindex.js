e2777592b345882350883bddb12e0cec857fd074
Astropianist <46462054+Astropianist@users.noreply.github.com>
2021-12-10 14:03:56 -0500

    Added basic docs directory for ReadTheDocs

---
+++ docs/build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:54,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to DustE\u2019s documentation!"],titleterms:{document:0,dust:0,indic:0,tabl:0,welcom:0}})
